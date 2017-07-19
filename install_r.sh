88cd89c016998b9bfdffa4c8f0bda1525f3f8253
David <cadillodavid@gmail.com>
2017-07-19 08:56:53 -0500

    Agregado nuevas líneas el script de instalación de R

--- install_r.sh
+++ install_r.sh
@@ -4,4 +4,11 @@ echo "deb http://cran.rstudio.com/bin/linux/ubuntu xenial/" | sudo tee -a /etc/a
 gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install r-base r-base-dev
+apt-get install r-base r-base-dev
+apt-get install r-cran-rjava
+export _JAVA_OPTIONS="-Xss2560k -Xmx2g"
+
+R()
+install.packages("wakefield")
+install.packages("xlsxjars")
+install.pacakages("xlsx")
